757a8946136bdc66b20c17930e30e868b94be289
Sophia <yuupiaa@gmail.com>
2018-08-09 10:32:09 -0700

    Add files via upload

---
+++ Exercise 5_1.cpp
@@ -0,0 +1,29 @@
+// Using a do-while loop to manage input
+#include <iostream>
+#include <vector>
+#include <array>
+//Exercise 5-1. Write a program that outputs the squares of the odd integers from 1 up to
+//a limit that is entered by the user.
+
+int main()
+{
+	std::cout<<"hello enter limit thanks" << std::endl;
+	int limit {};
+	std::cin>>limit;
+	std::cout<<limit<<std::endl;
+
+	if(limit%2){
+		std::cout<<"The squares of the odd integers ";
+		for(int i{1}, temp {limit}; i<=temp; i+=2){
+			std::cout << i * i << " ";
+
+		}
+	}
+	else{
+		std::cout<<"The squares of the odd integers ";
+		for(int i{1}, temp {limit-1}; i<=temp; i+=2){
+					std::cout << i * i << " ";
+		}
+	}
+
+}
